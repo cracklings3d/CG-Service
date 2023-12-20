c1a7d33721d09f82b52defe6ce9d341503f87cd7
cracklings3d <39695148+cracklings3d@users.noreply.github.com>
2023-12-20 17:27:37 +0800

    gRPC impl

--- src/Cgs_server.cpp
+++ src/Cgs_server.cpp
@@ -1,1 +1,9 @@
 #include "Cgs_server.h"
+
+grpc::Status cgs::Cgs_server::GetPointSet(
+  grpc::ServerContext *  context,
+  const Point_set_query *request,
+  Point_set *            response
+) {
+  return Service::GetPointSet(context, request, response);
+}
--- src/Cgs_server.h
+++ src/Cgs_server.h
@@ -2,10 +2,17 @@
 #define CGS_SERVER_H
 
 #include <grpc++/grpc++.h>
+#include <service.pb.h>
+#include <service.grpc.pb.h>
 
 
 namespace cgs {
-grpc::Service::AsyncService* get_service();
+class Cgs_server final : public Point_set_service::Service {
+  grpc::Status GetPointSet(
+    grpc::ServerContext *  context,
+    const Point_set_query *request,
+    Point_set *            response) override;
+};
 }
 
 
